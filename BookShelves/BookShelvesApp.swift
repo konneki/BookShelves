b37a4f81213b6a30d7823b9b26957568f3f8f5a4
Konneki <konnekdaniel@gmail.com>
2024-10-20 21:09:10 +0200

    Initial Commit

---
+++ BookShelves/BookShelvesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelvesApp.swift
+//  BookShelves
+//
+//  Created by Daniel Konnek on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookShelvesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
