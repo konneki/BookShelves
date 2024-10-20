fdb103d467d77c791ff90f20be82412c7ae28410
Konneki <konnekdaniel@gmail.com>
2024-10-20 21:12:09 +0200

    - initial files layout

--- BookShelves/BookShelvesApp.swift
+++ BookShelves/BookShelvesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BookShelvesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
--- BookShelves/Views/MainView.swift
+++ BookShelves/Views/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  BookShelves
 //
 //  Created by Daniel Konnek on 20.10.2024.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
